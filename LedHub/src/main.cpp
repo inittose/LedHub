3a6ae43ededa6dc361627dd5594749d6e84a88ff
Konstantin Polushvayko <pol.konnik@gmail.com>
2025-04-11 09:37:02 +0700

    feature(main, platformio.ini): Настроен platformio перед работой
    - Настроен monitor speed для Serial monitor
    - Настроен upload speed

--- LedHub/src/main.cpp
+++ LedHub/src/main.cpp
@@ -5,11 +5,15 @@ int myFunction(int, int);
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(115200);
   int result = myFunction(2, 3);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  Serial.println("Hello!");
+  delay(1000);
 }
 
 // put function definitions here:
